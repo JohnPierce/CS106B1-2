c014515648eda1862d94d6fc8a935109993e5252
John Pierce <jjvppierce@sbcglobal.net>
2016-09-18 09:48:05 -0700

    Initial Commit

---
+++ CS106B-ProblemSet1-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS106B-ProblemSet1-2
+//
+//  Created by John Pierce on 9/18/16.
+//  Copyright © 2016 JohnPierce. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
